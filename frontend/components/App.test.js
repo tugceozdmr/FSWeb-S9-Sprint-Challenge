d8531f8f68c502b5c4568e3e9437e79ed62d2d91
tugce <tugceozdemiir@hotmail.com>
2022-12-27 15:54:23 +0300

    tugce

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,61 @@
+import React from "react";
+import {
+  fireEvent,
+  getByText,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import AppFunctional from "./AppFunctional";
+
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+
+// let email, submit;
+
+// const gerekliDegiskenler = () => {
+//   email = screen.getByTestId("email-input");
+//   submit = screen.getByTestId("submit-button");
+// };
+
+test("hata olmadan render ediliyor", () => {
+  render(<AppFunctional />);
+});
+
+test("kullanıcı email girebiliyor", () => {
+  render(<AppFunctional />);
+  const email = screen.getByTestId("email-input");
+  fireEvent.change(email, { target: { value: "aaa@gmail.com" } });
+  expect(email.value).toBe("aaa@gmail.com");
+});
+
+test("aşağı butonu çalışıyor ve hata geliyor", () => {
+  render(<AppFunctional />);
+  const dbutton = screen.getByTestId("down-button");
+  fireEvent.click(dbutton);
+  fireEvent.click(dbutton);
+  expect(screen.getByText("aşağı gidemezsiniz")).toBeInTheDocument();
+});
+test("yukarı butonu çalışıyor ve hata geliyor", () => {
+  render(<AppFunctional />);
+  const upbutton = screen.getByTestId("up-button");
+  fireEvent.click(upbutton);
+  fireEvent.click(upbutton);
+  expect(screen.getByText("yukarı gidemezsiniz")).toBeInTheDocument();
+});
+test("sağa gitme butonu çalışıyor ve hata geliyor", () => {
+  render(<AppFunctional />);
+  const rbutton = screen.getByTestId("right-button");
+  fireEvent.click(rbutton);
+  fireEvent.click(rbutton);
+  expect(screen.getByText("Sağa gidemezsiniz.")).toBeInTheDocument();
+});
+
+test("sola gitme butonu çalışıyor ve hata geliyor", () => {
+  render(<AppFunctional />);
+  const lbutton = screen.getByTestId("left-button");
+  fireEvent.click(lbutton);
+  fireEvent.click(lbutton);
+  expect(screen.getByText("Sola gidemezsiniz.")).toBeInTheDocument();
+});
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,141 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
-// önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+//(y-1)*3+(x-1)
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
-  function getXY() {
-    // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
-    // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+  //1)konumu state'te tuttum.
+  const [konum, setKonum] = useState([2, 2]);
+
+  //2hamle sayısını tutmak için ikinci state
+
+  const [hamleSayisi, setHamleSayisi] = useState(0);
+
+  //hata mesajı için;
+  const [mesaj, setMesaj] = useState("");
+  const [email, setEmail] = useState("");
+
+  const konumAsIndex = (konum[1] - 1) * 3 + konum[0] - 1;
+  console.log(konum, konumAsIndex);
+
+  function sagaGit() {
+    if (konum[0] < 3) {
+      setKonum([konum[0] + 1, konum[1]]);
+      setHamleSayisi(hamleSayisi + 1);
+    } else {
+      setMesaj("Sağa gidemezsiniz.");
+    }
   }
 
-  function getXYMesaj() {
-    // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
-    // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
+  function solaGit() {
+    if (konum[0] > 1) {
+      setKonum([konum[0] - 1, konum[1]]);
+      setHamleSayisi(hamleSayisi + 1);
+    } else {
+      setMesaj("Sola gidemezsiniz.");
+    }
+  }
+
+  function asagiGit() {
+    if (konum[1] < 3) {
+      setKonum([konum[0], konum[1] + 1]);
+      setHamleSayisi(hamleSayisi + 1);
+    } else {
+      setMesaj("aşağı gidemezsiniz");
+    }
+  }
+
+  function yukariGit() {
+    if (konum[1] > 1) {
+      setKonum([konum[0], konum[1] - 1]);
+      setHamleSayisi(hamleSayisi + 1);
+    } else {
+      setMesaj("yukarı gidemezsiniz");
+    }
   }
 
   function reset() {
-    // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
-  }
-
-  function sonrakiIndex(yon) {
-    // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
-    // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
-    // şu anki indeksi değiştirmemeli.
-  }
-
-  function ilerle(evt) {
-    // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
-    // ve buna göre state i değiştirir.
+    setKonum([2, 2]);
+    setHamleSayisi(0);
+    setMesaj("");
+    setEmail("");
   }
 
   function onChange(evt) {
-    // inputun değerini güncellemek için bunu kullanabilirsiniz
+    setEmail(evt.target.value);
   }
 
-  function onSubmit(evt) {
-    // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
-  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const payload = {
+      x: konum[0],
+      y: konum[1],
+      steps: hamleSayisi,
+      email: email,
+    };
+    console.log(payload);
+    axios
+      .post("http://localhost:9000/api/result", payload)
+      .then((res) => {
+        console.log(res.data);
+        reset();
+      })
+      .catch((error) => {
+        console.log("Hatalı payload", error);
+      });
+  };
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        {console.log(konum)}
+        <h3 id="coordinates">Koordinatlar ({konum.join(", ")})</h3>
+        <h3 id="steps">{hamleSayisi} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div
+            key={idx}
+            className={`square${idx === konumAsIndex ? " active" : ""}`}
+          >
+            {idx === konumAsIndex ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{mesaj}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={solaGit} data-testid="left-button">
+          SOL
+        </button>
+        <button id="up" onClick={yukariGit} data-testid="up-button">
+          YUKARI
+        </button>
+        <button id="right" onClick={sagaGit} data-testid="right-button">
+          SAĞ
+        </button>
+        <button id="down" onClick={asagiGit} data-testid="down-button">
+          AŞAĞI
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
-        <input id="submit" type="submit"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="email"
+          type="email"
+          placeholder="email girin"
+          onChange={onChange}
+          data-testid="email-input"
+        ></input>
+        <input id="submit" type="submit" data-testid="submit-button"></input>
       </form>
     </div>
-  )
+  );
 }
